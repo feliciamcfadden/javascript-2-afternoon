48c05ebeaa686939cd76b1c7dd58ba6c5d1ee05b
felicia mcfadden <feliciamcfadden1@gmail.com>
2019-08-14 17:10:11 -0600

    javascript-2-afternoon

--- practice.js
+++ practice.js
@@ -16,6 +16,13 @@
 
 //Code here
 
+let me = {
+  name: 'Felicia',
+  age: 23
+};
+
+
+
 ////////// PROBLEM 2 //////////
 
 /*
@@ -25,12 +32,24 @@
 
 //Code here
 
+let favoriteThings = {
+  band: 'AJR',
+  food: 'Takis',
+  person: 'Taylor Swift',
+  book: 'Harry Potter',
+  movie: 'Avengers End game',
+  holiday: 'Halloween'
+};
+
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
 //Code here
+favoriteThings.car ='Sabaru'
+favoriteThings.brand = 'none'
+
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
@@ -38,6 +57,8 @@
 */
 
 //Code here
+favoriteThings.food ='Chicken Nuggets'
+favoriteThings.book= 'Harry Potter'
 
 ////////// PROBLEM 3 //////////
 
@@ -50,12 +71,21 @@
 */
 
 //Code here
+let backPack = {
+
+}
+
+let item = 'firstPocket'
+backPack['firstPocket'] = 'chapstick'
 
 /*
   After you do the above, alert your entire backPack object.
+
 */
 
 //Code here
+alert(backPack)
+
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -64,6 +94,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -85,6 +116,8 @@ var user2 = {
 */
 
 //Code Here
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -96,7 +129,9 @@ var user2 = {
 */
 
 //Code Here
-
+let methodCollection = {
+  
+}
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
